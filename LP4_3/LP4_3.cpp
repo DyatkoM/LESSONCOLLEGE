9957112406bbe2d81a4c0f73e7f0bb7a60b65b2c
maria <akk05827@gmail.com>
2022-09-25 00:27:10 +0300

    доделать

---
+++ LP4_3/LP4_3.cpp
@@ -0,0 +1,33 @@
+﻿/*Пользователь вводит натуральное число. Определите, все ли цифры
+во введенном числе четные. Получите наибольшее целое k, при
+котором 3^k меньше введенного числа.*/
+
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int number;
+	int k = 0;
+	int i = 3;
+	cout << "num: ";
+	cin >> number;
+	
+	while (i < number) {
+		
+		i = pow(i, k);
+		k++;
+	}
+	cout << k-1 << endl;
+	while (number > 0)  {
+		k = number % 10;
+		if (number % 2 == 0) {
+			cout << "+" << endl;
+		}
+		else
+			cout << "-"<<endl;
+		number /= 10;
+	}
+	
+}
+
