24d788c5a84a8aaefb8d5a9bbb8cae48e81c9ea8
maria <akk05827@gmail.com>
2022-09-26 21:10:06 +0300

    /*Пользователь вводит натуральное число а. Найти k простых чисел больших а и, если возможно, k простых чисел меньших а. a и k вводит пользователь. Если это невозможно вывести соответствующее сообщение.*/

---
+++ LP4_20/LP4_20.cpp
@@ -0,0 +1,55 @@
+﻿/*Пользователь вводит натуральное число а. Найти k простых чисел
+больших а и, если возможно, k простых чисел меньших а. a и k вводит
+пользователь. Если это невозможно вывести соответствующее
+сообщение.*/
+
+#include <iostream>
+using namespace std;
+int isprime(int num)
+{
+
+	if ((num * num) % 24 == 1)
+	{
+		return true;
+	}
+
+	return false;
+
+}
+int main()
+{
+	int a;
+	int k;
+	cout << "a:";
+	cin >> a;
+	cout << "k:";
+	cin >> k;
+	int s = a;
+	int d = k;
+	while (k != 0) {
+		if (isprime(a)) {
+			cout << a << "+";
+			k--;
+			a++;
+		}
+		else
+			a++;
+	}
+	cout << endl;
+	while (d != 0) {
+		if (isprime(s)) {
+			if (s <=2) {
+				cout << "Error";
+				break;
+			}
+			cout << s<<"-";
+			d--;
+			s--;
+		}
+		else
+			s--;
+
+	}
+}
+
+
